f4bc095897686cb894709d15338066c9f96e5c9e
audreychan <league@WTS14-4.attlocal.net>
2019-03-05 18:29:00 -0800

    day 2
    day 2

--- src/section1/Tash Me/sketch_190226a/sketch_190226a.pde
+++ src/section1/Tash Me/sketch_190226a/sketch_190226a.pde
@@ -2,4 +2,9 @@ PImage mustache;
 PImage friend;
 void setup() {
   friend=loadImage ("friend.jpg");
-  size (800,600
+  size (800,600);
+  friend.resize (width,height);
+  mustache=loadImage("mustache.png");}
+  void draw(){
+  background(friend);
+  image(mustache, mouseX, mouseY);}
--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -1,40 +1,38 @@
 package section2;
 
-import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
-	
-	// 2. Create a new Robot
+
+	Robot hi = new Robot();
 
 	void go() {
-		// 4. Make the robot move as fast as possible
+		hi.setSpeed(400);
+		hi.penDown();
+		for (int i = 0; i < 4; i++) {
+			hi.setRandomPenColor();
+			hi.setPenWidth(5);
+		}
 
-		// 5. Set the pen width to 5
-
-		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
-			// 7. Set the pen color to random
-	
-			// 1. Call the drawSquare() method
-	
-			// 8. Turn the robot 90 degrees to the right
+		// 1. Call the drawSquare() method
+		drawSquare();
+		// 8. Turn the robot 90 degrees to the right
 
 	}
 
-	
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
+		// JOptionPane.showMessageDialog(null, "yay! you called the drawSquare()
+		// method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
-		
+		for (int i = 0; i < 4; i++) {
+			hi.move(100);
+			hi.turn(90);
+
+		}
 	}
 
 	public static void main(String[] args) {
 		new FourSquare().go();
+
 	}
-
 }
-
-
-
